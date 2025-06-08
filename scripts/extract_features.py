a83020ffd29dedb0bcf0b93744321341d65d0f34
BorkenWill <143086755+BorkenWill@users.noreply.github.com>
2025-06-08 19:24:17 +0800

    Create extract_features.py

---
+++ scripts/extract_features.py
@@ -0,0 +1,20 @@
+import numpy as np
+import librosa
+
+def extract_features(file_path):
+    y, sr = librosa.load(file_path, duration=30)
+
+    mfccs = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=13)
+    mfccs_mean = np.mean(mfccs.T, axis=0)
+
+    chroma = librosa.feature.chroma_stft(y=y, sr=sr)
+    chroma_mean = np.mean(chroma.T, axis=0)
+
+    contrast = librosa.feature.spectral_contrast(y=y, sr=sr)
+    contrast_mean = np.mean(contrast.T, axis=0)
+
+    y_harmonic = librosa.effects.harmonic(y)
+    tonnetz = librosa.feature.tonnetz(y=y_harmonic, sr=sr)
+    tonnetz_mean = np.mean(tonnetz.T, axis=0)
+
+    return np.hstack([mfccs_mean, chroma_mean, contrast_mean, tonnetz_mean])
